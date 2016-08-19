0a603bf3ae383142edfb967f9bb9fa868efd63a2
Pedro Pereirinha <pereirinhaped@gmail.com>
2016-08-19 11:46:34 +0100

    @IBOutlets added

--- RetroCalculator/CalcVC.swift
+++ RetroCalculator/CalcVC.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class CalcVC: UIViewController {
 
+	// MARK: IBOutlets
+	//Counter
+	@IBOutlet weak var counterLbl: UILabel!
+	
+	// Numpad:
+	@IBOutlet weak var nineBtn: UIButton!
+	@IBOutlet weak var eightBtn: UIButton!
+	@IBOutlet weak var sevenBtn: UIButton!
+	@IBOutlet weak var sixBtn: UIButton!
+	@IBOutlet weak var fiveBtn: UIButton!
+	@IBOutlet weak var fourBtn: UIButton!
+	@IBOutlet weak var threeBtn: UIButton!
+	@IBOutlet weak var twoBtn: UIButton!
+	@IBOutlet weak var oneBtn: UIButton!
+	@IBOutlet weak var zeroBtn: UIButton!
+	
+	// Operators:
+	@IBOutlet weak var divideBtn: UIButton!
+	@IBOutlet weak var multiplyBtn: UIButton!
+	@IBOutlet weak var subBtn: UIButton!
+	@IBOutlet weak var addBtn: UIButton!
+	@IBOutlet weak var equalBtn: UIButton!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
 
 }
 
